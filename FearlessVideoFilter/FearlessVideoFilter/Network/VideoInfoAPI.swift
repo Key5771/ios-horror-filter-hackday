2bff75bf194861b136f7a4eaf4871562b5a7a556
Key5771 <ksj57715@hanmail.net>
2020-05-13 11:31:45 +0900

    API 받아오는 과정 추가. 현재는 하나의 클래스에서 이뤄지는 과정을 분리해야함.

---
+++ FearlessVideoFilter/FearlessVideoFilter/Network/VideoInfoAPI.swift
@@ -0,0 +1,68 @@
+//
+//  VideoInfoAPI.swift
+//  FearlessVideoFilter
+//
+//  Created by 김기현 on 2020/05/11.
+//  Copyright © 2020 Hackday2020. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+// MARK: - APIStruct
+struct APIStruct: Codable {
+    var header: Header
+    var body: Body
+}
+
+// MARK: - HEADER
+struct Header: Codable {
+    var code: Int
+    var message: String
+}
+
+// MARK: - BODY
+struct Body: Codable {
+    var clips: [Clip]
+    var hasNext: Bool?
+}
+
+
+// MARK: - CLIP
+struct Clip: Codable {
+    var clipNo: Int
+    var title: String
+    var thumbnailUrl: String
+    var channelEmblemUrl: String
+    var channelName: String
+    var duration: Int
+}
+
+// MARK: - REQUEST
+// TODO: - 클래스 분리해야함.
+class NetworkRequest {
+    static let shared: NetworkRequest = NetworkRequest()
+    let baseUrl = "http://27.96.131.40:10001/api/v1/clip"
+    
+    enum API: String {
+        case videoInfo = "/list"
+        case filterInfo = "/filter/list"
+    }
+    
+    enum NetworkError: Error {
+        case http404
+    }
+    
+    func requestVideoInfo(api: API, method: Alamofire.HTTPMethod, parameters: Parameters? = nil, completion handler: @escaping ([Clip]) -> Void) {
+        // responseDecodable
+        AF.request(baseUrl+api.rawValue, method: .get, parameters: parameters).responseDecodable(of: APIStruct.self) { (response) in
+            switch response.result {
+            case .success(let object):
+                let data = object.body.clips
+                handler(data)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
--- FearlessVideoFilter/FearlessVideoFilter/UI/MainViewController.swift
+++ FearlessVideoFilter/FearlessVideoFilter/UI/MainViewController.swift
@@ -15,6 +15,9 @@ class MainViewController: UIViewController {
     
     let dummyArr: [VideoInfo] = VideoInfo.makeDummyData()
     
+    // api를 통해 받아온 데이터를 저장하는 배열
+    var infoArr: [Clip] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
@@ -23,6 +26,11 @@ class MainViewController: UIViewController {
         let nibName = UINib(nibName: "VideoCollectionViewCell", bundle: nil)
         collectionView.register(nibName, forCellWithReuseIdentifier: "VideoCollectionViewCell")
         
+        NetworkRequest.shared.requestVideoInfo(api: .videoInfo, method: .get) { (response) in
+            // 현재 infoArr에 저장되는 부분이 늦게 실행되기 때문에 reloadData()를 해주어야 셀에서 표현가능
+            self.infoArr = response
+//            self.collectionView.reloadData()
+        }
     }
     
     // 화면 회전 시 cell size가 업데이트되지 않는 현상 방지
@@ -48,9 +56,11 @@ extension MainViewController: UICollectionViewDataSource {
         }
         
         let infoData = dummyArr[indexPath.row]
+        // 기존의 dummyArr대신 infoArr을 사용하면 됩니다.
+//        let infoData = infoArr[indexPath.row]
         
         let imageURL = getURL(dummyArr[indexPath.row].thumbnailName)
-        
+
         do {
             if let thumbnailURL = Bundle.main.url(forResource: imageURL[0], withExtension: imageURL[1]) {
                 let data = try Data(contentsOf: thumbnailURL)
@@ -74,20 +84,18 @@ extension MainViewController: UICollectionViewDelegate, UICollectionViewDelegate
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        let infoData = dummyArr[indexPath.row]
-        let videoName = getURL(dummyArr[indexPath.row].videoName)
+        let infoData = dummyArr[indexPath.row]
+        let videoName = getURL(infoData.videoName)
         if let videoURL = Bundle.main.url(forResource: videoName[0], withExtension: videoName[1]) {
             // 블러처리 해주는 기초적인 클래스 구현 - 현재는 한 구간만 블러 가능
             let filteredItem = FilteredPlayerItem(videoURL: videoURL)
             
             // 블러 시작구간을 start, 끝구간을 end로 설정
-            guard let start = Double(dummyArr[indexPath.row].start),
-                let end = Double(dummyArr[indexPath.row].end) else { return }
+            guard let start = Double(infoData.start),
+                let end = Double(infoData.end) else { return }
             
             filteredItem.blur(from: start, to: end, animationRate: 1.0)
             let player = AVPlayer(playerItem: filteredItem.playerItem)
-            
-//            let player = AVPlayer(url: videoURL)
             
             let controller = AVPlayerViewController()
             controller.player = player
